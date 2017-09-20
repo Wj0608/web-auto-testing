e37a19242ca1aa04caa5643c6554d630f97c82bc
yangmengying <yangmengying8977@dingtalk.com>
2017-09-20 17:18:30 +0800

    数字交易所改为数字钱包更新

---
+++ testcase/test_ForgetWithdrawPwd.py
@@ -0,0 +1,60 @@
+#coding=utf-8
+
+
+from time import sleep
+from public.common import mytest
+from public.common import publicfunction
+from public.pages import DAE_PersonalCenter
+from config import DAE_SuccessMessage,DAE_ErrorMessage
+
+from testcase import test_RegisterAndLogin
+
+class TestWithdrawPwd(mytest.MyTest):
+
+    # 修改提现密码
+    def test_a_ChangeWithdrawPwd(self):
+        test_RegisterAndLogin.TestHuiyin.test_b_login_WrongPassword(self)
+        personalPage = DAE_PersonalCenter.PersoalCenter(self.dr)
+        personalPage.click_PersonalCenter()
+        personalPage.click_ChangeTradePwd()
+        personalPage.type_OldTradePwd('123456')
+        personalPage.type_NewTradePWd('123456')
+        personalPage.type_ConfirmTradePwd('123456')
+        personalPage.click_Submit()
+        self.assertTrue(self.dr.get_element(DAE_SuccessMessage.ChangePwdSuccess).is_displayed())
+
+    # 忘记提币密码
+    def test_b_ForgetWithdrawPwd(self):
+        test_RegisterAndLogin.TestHuiyin.test_b_login_WrongPassword(self)
+        persoanlPage = DAE_PersonalCenter.PersoalCenter(self.dr)
+        persoanlPage.click_ForgetTradePwd()
+        persoanlPage.SendVcode('123456')
+        persoanlPage.click_Submit()
+        persoanlPage.type_NewTradePWd('123456')
+        persoanlPage.type_ConfirmTradePwd('123456')
+        persoanlPage.click_Submit()
+        self.
+
+    # 修改提现密码-原密码错误
+    def test_a_ChangeWithdrawPwdWrongOldPwd(self):
+        test_RegisterAndLogin.TestHuiyin.test_b_login_WrongPassword(self)
+        personalPage = DAE_PersonalCenter.PersoalCenter(self.dr)
+        personalPage.click_PersonalCenter()
+        personalPage.click_ChangeTradePwd()
+        personalPage.type_OldTradePwd('7894561')
+        personalPage.type_NewTradePWd('123456')
+        personalPage.type_ConfirmTradePwd('123456')
+        personalPage.click_Submit()
+        self.assertTrue(self.dr.get_element(DAE_ErrorMessage.OldWithdrawPwd_Error).is_displayed())
+
+    # 修改提现密码-确认密码与新密码不同
+    def test_a_DifferentConfirmPwd(self):
+       test_RegisterAndLogin.TestHuiyin.test_b_login_WrongPassword(self)
+        personalPage = DAE_PersonalCenter.PersoalCenter(self.dr)
+        personalPage.click_PersonalCenter()
+        personalPage.click_ChangeTradePwd()
+        personalPage.type_OldTradePwd('7894561')
+        personalPage.type_NewTradePWd('123456')
+        personalPage.type_ConfirmTradePwd('123456')
+        personalPage.click_Submit()
+        self.assertTrue(self.dr.get_element(DAE_ErrorMessage).is_displayed())
